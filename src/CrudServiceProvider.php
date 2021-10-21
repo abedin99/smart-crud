53ba15b754b418af5a9e446d4eff1186bc20a0f3
abedin99 <pjoy813@gmail.com>
2021-10-21 13:08:56 +0600

    First commit

---
+++ src/CrudServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Abedin99\CrudBangla;
+
+use Illuminate\Support\ServiceProvider;
+
+class CrudServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
